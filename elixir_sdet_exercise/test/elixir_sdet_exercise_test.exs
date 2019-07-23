92f26356afc7ca115c90d60e49ba32a5761fc61c
Jason McLean <jasonamclean@hotmail.com>
2019-07-23 00:13:19 -0600

    add fields to fill for account creation

--- elixir_sdet_exercise/test/elixir_sdet_exercise_test.exs
+++ elixir_sdet_exercise/test/elixir_sdet_exercise_test.exs
@@ -6,8 +6,27 @@ defmodule ElixirSdetExerciseTest do
   # Start hound session and destroy when tests are run
   hound_session()
 
-  test "goes to facebook" do
-    navigate_to "http://facebook.com"
-    IO.inspect page_title()
+  test "create facebook account" do
+    navigate_to "https://www.facebook.com"
+
+  first_name_element = find_element(:id, "u_0_p")
+  fill_field(first_name_element, "sponge")
+
+  last_name_element = find_element(:id, "u_0_r")
+  fill_field(last_name_element, "pants")
+
+  email_element = find_element(:name, "reg_email__")
+  fill_field(email_element, "")
+
+  password_element = find_element(:name, "reg_passwd__")
+  fill_field(password_element, "")
+
+  gender_element = find_element(:id, "u_0_8")
+  click(gender_element)
+
+  submit_element(gender_element)
+
+  delete_cookies()
   end
+
 end
